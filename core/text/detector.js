98a2f45200bb408e42c2cc82224e24021ea0c3df
Lsfelt Ulv <lsfelt-ulv@outlook.com>
2021-01-20 16:49:32 +0800

    Happy new year 2021!
    Change(s):
      [1] Happy new year 2021!
    
    Contributor(s):
      [1] Lsfelt Ulv <lsfelt-ulv@outlook.com>

--- core/text/detector.js
+++ core/text/detector.js
@@ -1,5 +1,5 @@
 //
-//  Copyright 2015 - 2020 The XRT Authors. All rights reserved.
+//  Copyright 2015 - 2021 The XRT Authors. All rights reserved.
 //  Use of this source code is governed by a BSD-style license that can be
 //  found in the LICENSE.md file.
 //
