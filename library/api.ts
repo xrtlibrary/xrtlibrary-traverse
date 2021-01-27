f2e85dd7f6660d777ff08a5862be9c6f87fa9702
Lsfelt Ulv <lsfelt-ulv@outlook.com>
2021-01-27 14:13:43 +0800

    Removed unused import in exported API file.
    Change(s):
      [1]  Removed unused import in exported API file.
    
    Contributor(s):
      [1] Varme <hackerlinx@outlook.com>

--- library/api.ts
+++ library/api.ts
@@ -4,13 +4,6 @@
 //  found in the LICENSE.md file.
 //
 
-//
-//  Imports
-//
-
-//  Imported modules.
-import CrTraverse = require("./../core/traverse");
-
 //  Export public APIs.
 export { Traverse as Traverse} from "./../core/traverse";
 export { WrapObject as WrapObject } from "./../core/traverse";
